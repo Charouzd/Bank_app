6a5ba9a86236d5254ded235ff1f1bce017fb5ffe
Charouzd <72192163+Charouzd@users.noreply.github.com>
2023-03-29 22:07:23 +0200

    Adding bank_logic
    add bank-logic to the list of apps

--- bank_app/settings.py
+++ bank_app/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'authentific',
+<<<<<<< Updated upstream
+=======
+    'bank_logic'
+>>>>>>> Stashed changes
     'django_crontab',
 ]
 
